280ca4001da9fbac20ae224fbd919f788da79cd6
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2023-11-09 05:52:10 +0000

    datatypes

---
+++ 1_basics/2_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat al JS code as newer version
+//alert(3+3) //we are using node.js not browser
+console.log(3+3) //code readability should be high
+console.log("Khushi")
+
+let name = "Khushi" //string datatype
+let age = 18 //numbers value
+let isLoggedIn = false //boolean datatype
+
+//number => 2 to power 53
+//bigint is used when the number s too big
+//string can be used in double cots ans single also
+//boolean => true or false
+//null => stand alone value, yeah ek datatype h, representation of empty value
+//null ek object h
+//undefined => mtlb variable define kari h pr value assign nhi kiya h
+//symbol => yeah react m kaam ata h, jab hum bahut sara componenet banate h or humko idenetify karna h yeah wala component unique h
+
+//object
+console.log(typeof "Khushi");
+console.log(typeof age);
