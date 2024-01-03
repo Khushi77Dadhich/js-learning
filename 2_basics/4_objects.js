b0e24b8638e5e0e12d65aeb1580a13995576120a
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2024-01-03 18:57:49 +0000

    object discussion

--- 2_basics/4_objects.js
+++ 2_basics/4_objects.js
@@ -5,7 +5,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+//console.log(tinderUser);
 
 const regularUser = {
     email: "dadhich@gmail.com",
@@ -17,4 +17,33 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname);
+//console.log(regularUser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+//const obj4 = { obj1, obj2 } 
+//const obj4 = Object.assign({}, obj1, obj2, obj3 ) //copy the value of all of the enumerable own properties from one or more source objects to a target object. return the target object.
+
+const obj4 = {...obj1, ...obj2, ...obj3} //we will maximum time use this for object
+//console.log(obj4);
+
+
+const users = [
+    {
+        id: 1, 
+        email: "k@gmail.com"
+    },
+    {
+        id: 1, 
+        email: "k@gmail.com"
+    },
+    {
+        id: 1, 
+        email: "k@gmail.com"
+    },
+]
+
+users[1].email
+console.log
