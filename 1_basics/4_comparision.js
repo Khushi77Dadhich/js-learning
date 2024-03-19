6bcc7f6d8e1fbc8b48cfd9dcf1a53a769028190e
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2024-03-19 05:58:36 +0000

    comparision

--- 1_basics/4_comparision.js
+++ 1_basics/4_comparision.js
@@ -8,11 +8,11 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0); //the reason is that an equality check == and comparision >< >= <= work differently.
+console.log(null > 0); //false
+console.log(null == 0); // false, the reason is that an equality check == and comparision >< >= <= work differently.
 console.log(null >= 0); //comparision convert null to a number, treating it as 0.that's why null >= 0 is true and null > 0 is false.
 
-console.log(undefined == 0);
+console.log(undefined == 0);  //false is output for all three
 console.log(undefined > 0);
 console.log(undefined < 0);
 
