413c72c9af2595280ecbde8e89a789da69663d78
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2024-03-19 06:34:01 +0000

    summary

--- 1_basics/datatypes_summary.js
+++ 1_basics/datatypes_summary.js
@@ -1,4 +1,4 @@
-//primitive
+//primitive datatype are call by value hote h whenever we call them the are call by value so whatever change we do it is done in that copied value 
 
 //7 types: String, Number, Boolean, null, undefined, Symbol, BigInt(jab bahut badi value ho)
 
@@ -7,14 +7,14 @@ const scoreValue = 100.3
 
 const isLoggedIn = false
 const outsideTemp = null
-let userEmail;
+let userEmail;  //it is undefined
 
-const id = Symbol('123')
+const id = Symbol('123')  
 const anotherId = Symbol('123')
 
-//console.log(id === anotherId); //same nhi hoga kyo ki symbol datatype m asa hi hota h
+console.log(id === anotherId); //same nhi hoga kyo ki symbol datatype m asa hi hota h
 
-//const bigNumber = 46813279465413467n
+//const bigNumber = 46813279465413467n after appling n it become bigint
 
 
 
@@ -41,7 +41,7 @@ console.log(typeof anotherId); //symbol datatype
 //*******************************************
 
 //Stack (primitive), Heap (non-primitive)
-//stack memory
+//stack memory:- copy milti hai
 let myYoutubename = "khushidadhich.com"
 
 let anothername = myYoutubename
@@ -49,7 +49,7 @@ anothername = "dadhich.com"
 
 console.log(myYoutubename);
 console.log(anothername);
-//heap memory
+//heap memory:- reference of original value
 let userOne = {
     email: "user@google.com",
     upi: "user@ybl"
